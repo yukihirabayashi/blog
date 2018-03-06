43111a480d3d1a69fa7498f8ee1df881046b4f64
Yuki Hirabayashi <hira_y@nttpc.co.jp>
2018-03-06 13:44:19 +0000

    init

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,8 +3,7 @@ class FavoritesController < ApplicationController
     def create
         
         favorite = current_user.favorites.create(blog_id: params[:blog_id])
-        
-
+         redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り登録しました"
     end
 
     def destroy
